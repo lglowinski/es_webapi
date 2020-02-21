7640632d3ca1bcbee26a944f972e2745220e7f07
Łukasz Głowiński <lukasz.glowinski@o2.pl>
2020-02-21 20:21:27 +0100

    Added required parameters

--- ExpertalSystem/Requests/CreateProblemRequest.cs
+++ ExpertalSystem/Requests/CreateProblemRequest.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using ExpertalSystem.Domain;
 
 namespace ExpertalSystem.Requests
 {
     public class CreateProblemRequest
     {
+        [Required]
         public string ProblemName { get; set; }
+        [Required]
         public List<QuestionRequest> Questions { get; set; } = new List<QuestionRequest>();
+        [Required]
         public IssueType IssueType { get; set; }
+        [Required]
         public string Solution { get; set; }
     }
 
     public class QuestionRequest
     {
+        [Required]
         public string QuestionName { get; set; }
+        [Required]
         public string Answer { get; set; }
     }
 }
